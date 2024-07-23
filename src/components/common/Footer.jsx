d0f4eee48c56f5cf233ed74194f81c6cefa2430d
wanjohi lewis <wanjohilewi7@gmail.com>
2024-07-23 12:39:21 +0300

    final changes

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const FooterComponent = () => {
+    return (
+        <div>
+            <footer className='footer'>
+                <span>Dev Lewis | All Right Reserved &copy; {new Date().getFullYear()} </span>
+            </footer>
+        </div>
+    )
+}
+
+export default FooterComponent
